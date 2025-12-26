4d17c49aabfcca5899e76a6633febc06370af79e
Anukriti-j <anu200307@gmail.com>
2025-12-26 11:36:33 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by coditas on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
